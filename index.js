55f6c0452eeb63c47358bcb81c77473b9b8faea8
Vincent Servio <vincentservio@gmail.com>
2020-08-29 23:42:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function fetchBooks() {
 fetch("https://anapioficeandfire.com/api/books")
 .then(res => res.json())
-.then(json => renderBooks(books))
+.then(books => renderBooks(books))
 }
 
 function renderBooks(books) {
